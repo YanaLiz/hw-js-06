41c2a5cb36b390ec2b10f95e301dca4370fa6d97
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-10-06 19:33:09 +0300

    homework

--- js/task-05.js
+++ js/task-05.js
@@ -8,14 +8,10 @@ const outputRef = document.querySelector("#name-output")
     const inputValue = event.target.value;
     outputRef.disabled = !event.target.input;
     outputRef.textContent = `${inputValue}`;
-  });
-
-
- inputRef.addEventListener("input",()=> {
-    if (inputRef.value) {
+     if (inputRef.value) {
       outputRef.textContent = input.value;
     } else {
       outputRef.textContent = "Anonymous";
     }
- });
+  });
 console.log(inputRef);
